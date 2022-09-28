267662720f77639eeacf1454a14bb8f760b1be9c
Jessica Lartey <coloneltiorkor@gmail.com>
2022-09-28 12:23:34 +0000

    newchanges

--- src/models/GasData.js
+++ src/models/GasData.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const { model, Schema } = mongoose;
 
 const GasDataSchema = new Schema({
-    gasConcentration: String,
-    fireValue: Boolean,
-    owner: {
-        ref: "user",
-        type: Schema.Types.ObjectId,
-    },
+  gasConcentration: String,
+  fireValue: Boolean,
+  createdAt: String,
+  owner: {
+    ref: "user",
+    type: Schema.Types.ObjectId,
+  },
 });
 
 module.exports = model("gasdata", GasDataSchema);
